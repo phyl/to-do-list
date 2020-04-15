361a5c9aff97253e369ae503f95a4ba9c3b4ab0c
Phyllis <pmhindurwa@gmail.com>
2020-04-15 11:51:44 +0200

    Remove line break

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,6 @@
 //Variables section
 const tweetList = document.getElementById('tweet-list')
 
-
-
-
-
 //Event Listeners section
 eventListeners();
 
@@ -21,10 +17,7 @@ function eventListeners() {
     //Document
     document.addEventListener('DOMContentLoaded', localStorageOnLoad);
 
-
 }
-
-
 
 //Functions section
 
@@ -42,9 +35,9 @@ function newTweet(e) {
 
     //Add text to the list
 
-    var li = document.createElement('li');
-    li = document.createElement('a');
-    br = document.createElement('br');
+    const li = document.createElement('li');
+    //li = document.createElement('a');
+    //br = document.createElement('br');
     li.textContent = tweet;
 
     //Add the remove button
@@ -52,17 +45,20 @@ function newTweet(e) {
 
     //Add items to the list
     tweetList.appendChild(li);
-    tweetList.appendChild(br);
+    //tweetList.appendChild(br);
 
     addTweetLocalStorage(tweet);
+
+    // Print the alert
+    alert('Tweet Added');
+
+    this.reset();
 }
 //Cancel task
-function cancelTweet(e) {
-    document.getElementById(tweetList)
-    e.target.style.textDecoration = "line-through";
+function cancelTweet(tweetList) {
+    tweetList.target.style.textDecoration = "line-through";
 
 }
-
 
 //Remove the tweet from DOm
 function removeTweet(e) {
@@ -101,8 +97,8 @@ function localStorageOnLoad() {
 
     //Loop through storage and then print the values
 
-    //tweets.forEach((tweet) {
-    Array.prototype.forEach.call(tweets, function (tweet) {
+    tweets.forEach(function(tweet) {
+    //Array.prototype.forEach.call(tweets, function (tweet) {
         //The remove button
         const removeBtn = document.createElement('a');
         removeBtn.classList = 'remove-tweet';
@@ -110,9 +106,9 @@ function localStorageOnLoad() {
 
         //Add text to the list
 
-        var li = document.createElement('li');
-        li = document.createElement('a');
-        br = document.createElement('br');
+        const li = document.createElement('li');
+        //li = document.createElement('a');
+        //br = document.createElement('br');
         li.textContent = tweet;
 
         //Add the remove button
@@ -120,7 +116,7 @@ function localStorageOnLoad() {
 
         //Add items to the list
         tweetList.appendChild(li);
-        tweetList.appendChild(br);
+       // tweetList.appendChild(br);
     });
 }
 
@@ -134,8 +130,8 @@ function removeTweetLocalStorage(tweet) {
 
     //Loop through the tweets and remove the tweet that's equal
 
-    //tweets.forEach((tweetLS, index) {
-    Array.prototype.forEach.call(tweets, function(tweetLS, index) {
+    tweets.forEach(function(tweetLS, index) {
+    //Array.prototype.forEach.call(tweets, function(tweetLS, index) {
         if(tweetDelete === tweetLS) {
             tweets.splice(index, 1);
         } 
